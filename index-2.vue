10c5af81f34b0cef4f15da4b0f768bf6d91ef576
Angele Kael <angelekaloiwork@gmail.com>
2024-07-31 12:08:57 +1100

    Add files via upload
    vue-js

---
+++ index-2.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  /**
+   * A description of the entire function.
+   *
+   * @return {Object} An object containing the data properties for the component.
+   */
+  data() {
+    return {
+      list: [
+        /* ... */
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list" ref="items">
+      {{ item }}
+    </li>
+  </ul>
+</template>
---
+++ script-2.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+      ToDoList:["teste","teste2"],
+      Finished:[],
+      Entrada:''
+    },
+    methods:{
+      
+    }})
